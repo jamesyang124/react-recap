b531b2d77d2c57eab2942df1451629106ab531d6
James Yang <james_yang@htc.com>
2019-04-21 11:08:06 +0800

    UPD. update redux state hydration

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -7,6 +7,13 @@ class SongList extends Component {
   }
 }
 
+// redux will hydrate redux state to react props with default value,
+// from reducer's first argument's default value,
+// then any action creator will suppose update its state later
+//
+// so you might see reducer init with [], but later update as {} instead
+// only action creator can implicitly trigger state change,
+// so reducer respond an updated state, and cause react re-rendered.
 const mapStateToProps = (reduxState) => {
   console.log(reduxState);
   return reduxState;
